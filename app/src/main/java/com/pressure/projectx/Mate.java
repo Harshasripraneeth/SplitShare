dfd3e7d799556edaed33f35122bdc90912ba810a
Harshasripraneeth <46346478+Harshasripraneeth@users.noreply.github.com>
2020-02-17 02:21:23 +0530

    no message

---
+++ app/src/main/java/com/pressure/projectx/Mate.java
@@ -0,0 +1,37 @@
+package com.pressure.projectx;
+
+public class Mate {
+private String name;
+private String tel;
+private int amount;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Mate(String name, String tel, int amount) {
+        this.name = name;
+        this.tel = tel;
+        this.amount = amount;
+    }
+}
